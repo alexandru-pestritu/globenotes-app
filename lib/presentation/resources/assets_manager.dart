2fb6f1d764e5d49284b8b63afa179405421ed04c
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-02-15 00:49:30 +0200

    Added assets for onboarding screens

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -2,4 +2,7 @@ const String IMAGE_PATH = "assets/images";
 
 class ImageAssets {
   static const String splashLogo = "$IMAGE_PATH/splash_logo.png";
+  static const String onboardingIllustration1 = "$IMAGE_PATH/onboarding_illustration_1.svg";
+  static const String onboardingIllustration2 = "$IMAGE_PATH/onboarding_illustration_2.svg";
+  static const String onboardingIllustration3 = "$IMAGE_PATH/onboarding_illustration_3.svg";
 }
--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,3 +1,12 @@
 class AppStrings {
   static const String noRouteFound = "No Route Found";
+  static const String onBoardingTitle1 = "Create Your Personal Travel Journals";
+  static const String onBoardingTitle2 = "Explore & Track Your Adventures";
+  static const String onBoardingTitle3 = "Print & Share Your Memories";
+  static const String onBoardingSubTitle1 =
+      "Document your journey with photos, stories, and locations. Build a timeline of unforgettable moments and pin them on the map!";
+  static const String onBoardingSubTitle2 =
+      "Keep a record of your travels with interactive maps and statistics. See all the countries, cities, and wonders you've visited at a glance!";
+  static const String onBoardingSubTitle3 =
+      "Turn your travel journals into beautifully designed PDFs, ready for printing or sharing. Choose from customizable templates for a unique keepsake!";
 }
