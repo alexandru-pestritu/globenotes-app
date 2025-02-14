2c9075d781be882ed048ebc56f6ceb889b778803
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-02-14 14:05:12 +0200

    Created application class

---
+++ lib/app/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class MyApp extends StatefulWidget {
+  MyApp._internal();
+  int appState = 0;
+  static final MyApp instance =
+      MyApp._internal();
+
+  factory MyApp() => instance;
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
