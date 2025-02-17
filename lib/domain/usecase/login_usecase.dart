33c2d3481258af0afc1dfc75f90659ea3fe59d4a
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-02-17 15:12:43 +0200

    Base and Login use cases

---
+++ lib/domain/usecase/login_usecase.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:globenotes/data/network/failure.dart';
+import 'package:globenotes/data/request/auth_request.dart';
+import 'package:globenotes/domain/model/authentication.dart';
+import 'package:globenotes/domain/repository/auth_repository.dart';
+import 'package:globenotes/domain/usecase/base_usecase.dart';
+
+class LoginUseCase implements BaseUseCase<LoginUseCaseInput, Authentication> {
+  final AuthRepository _repository;
+
+  LoginUseCase(this._repository);
+
+  @override
+  Future<Either<Failure, Authentication>> execute(
+      LoginUseCaseInput input) async {
+    return await _repository
+        .login(LoginRequest(input.email, input.password));
+  }
+}
+
+class LoginUseCaseInput {
+  String email;
+  String password;
+
+  LoginUseCaseInput(this.email, this.password);
+}
