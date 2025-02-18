5b1b4c6049a492511f4e923786da009670db368d
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-02-18 14:19:02 +0200

    Login view model

--- lib/presentation/common/freezed_data_classes.freezed.dart
+++ lib/presentation/common/freezed_data_classes.freezed.dart
@@ -17,7 +17,7 @@ final _privateConstructorUsedError = UnsupportedError(
 
 /// @nodoc
 mixin _$LoginObject {
-  String get userName => throw _privateConstructorUsedError;
+  String get email => throw _privateConstructorUsedError;
   String get password => throw _privateConstructorUsedError;
 
   /// Create a copy of LoginObject
@@ -34,7 +34,7 @@ abstract class $LoginObjectCopyWith<$Res> {
     $Res Function(LoginObject) then,
   ) = _$LoginObjectCopyWithImpl<$Res, LoginObject>;
   @useResult
-  $Res call({String userName, String password});
+  $Res call({String email, String password});
 }
 
 /// @nodoc
@@ -51,13 +51,13 @@ class _$LoginObjectCopyWithImpl<$Res, $Val extends LoginObject>
   /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
   @override
-  $Res call({Object? userName = null, Object? password = null}) {
+  $Res call({Object? email = null, Object? password = null}) {
     return _then(
       _value.copyWith(
-            userName:
-                null == userName
-                    ? _value.userName
-                    : userName // ignore: cast_nullable_to_non_nullable
+            email:
+                null == email
+                    ? _value.email
+                    : email // ignore: cast_nullable_to_non_nullable
                         as String,
             password:
                 null == password
@@ -79,7 +79,7 @@ abstract class _$$LoginObjectImplCopyWith<$Res>
   ) = __$$LoginObjectImplCopyWithImpl<$Res>;
   @override
   @useResult
-  $Res call({String userName, String password});
+  $Res call({String email, String password});
 }
 
 /// @nodoc
@@ -95,12 +95,12 @@ class __$$LoginObjectImplCopyWithImpl<$Res>
   /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
   @override
-  $Res call({Object? userName = null, Object? password = null}) {
+  $Res call({Object? email = null, Object? password = null}) {
     return _then(
       _$LoginObjectImpl(
-        null == userName
-            ? _value.userName
-            : userName // ignore: cast_nullable_to_non_nullable
+        null == email
+            ? _value.email
+            : email // ignore: cast_nullable_to_non_nullable
                 as String,
         null == password
             ? _value.password
@@ -114,16 +114,16 @@ class __$$LoginObjectImplCopyWithImpl<$Res>
 /// @nodoc
 
 class _$LoginObjectImpl implements _LoginObject {
-  _$LoginObjectImpl(this.userName, this.password);
+  _$LoginObjectImpl(this.email, this.password);
 
   @override
-  final String userName;
+  final String email;
   @override
   final String password;
 
   @override
   String toString() {
-    return 'LoginObject(userName: $userName, password: $password)';
+    return 'LoginObject(email: $email, password: $password)';
   }
 
   @override
@@ -131,14 +131,13 @@ class _$LoginObjectImpl implements _LoginObject {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$LoginObjectImpl &&
-            (identical(other.userName, userName) ||
-                other.userName == userName) &&
+            (identical(other.email, email) || other.email == email) &&
             (identical(other.password, password) ||
                 other.password == password));
   }
 
   @override
-  int get hashCode => Object.hash(runtimeType, userName, password);
+  int get hashCode => Object.hash(runtimeType, email, password);
 
   /// Create a copy of LoginObject
   /// with the given fields replaced by the non-null parameter values.
@@ -150,11 +149,11 @@ class _$LoginObjectImpl implements _LoginObject {
 }
 
 abstract class _LoginObject implements LoginObject {
-  factory _LoginObject(final String userName, final String password) =
+  factory _LoginObject(final String email, final String password) =
       _$LoginObjectImpl;
 
   @override
-  String get userName;
+  String get email;
   @override
   String get password;
 
---
+++ lib/presentation/login/login_viewmodel.dart
@@ -0,0 +1,145 @@
+import 'dart:async';
+import 'package:globenotes/presentation/base/base_viewmodel.dart';
+import 'package:globenotes/presentation/common/freezed_data_classes.dart';
+import 'package:globenotes/presentation/resources/values_manager.dart';
+
+class LoginViewModel extends BaseViewModel
+    with LoginViewModelInputs, LoginViewModelOutputs {
+  final StreamController _emailStreamController =
+      StreamController<String>.broadcast();
+
+  final StreamController _passwordStreamController =
+      StreamController<String>.broadcast();
+
+  final StreamController _isAllInputsValidStreamController =
+      StreamController<void>.broadcast();
+
+  final StreamController<bool> _passwordVisibilityStreamController =
+      StreamController<bool>.broadcast();
+
+  final StreamController<bool> _emailIconStreamController =
+      StreamController<bool>.broadcast();
+
+  var loginObject = LoginObject("", "");
+
+  bool _isPasswordHidden = true;
+
+  LoginViewModel();
+
+  @override
+  void dispose() {
+    _emailStreamController.close();
+    _passwordStreamController.close();
+    _isAllInputsValidStreamController.close();
+    _passwordVisibilityStreamController.close();
+    _emailIconStreamController.close();
+  }
+
+  @override
+  void start() {}
+
+  @override
+  Sink get inputPassword => _passwordStreamController.sink;
+
+  @override
+  Sink get inputEmail => _emailStreamController.sink;
+
+  @override
+  Sink get inputIsAllInputValid => _isAllInputsValidStreamController.sink;
+
+  @override
+  Sink get inputPasswordVisibility => _passwordVisibilityStreamController.sink;
+
+  @override
+  Sink get inputEmailIcon => _emailIconStreamController.sink;
+
+  @override
+  login() {}
+
+  @override
+  setPassword(String password) {
+    inputPassword.add(password);
+    loginObject = loginObject.copyWith(password: password);
+    _validate();
+  }
+
+  @override
+  setEmail(String email) {
+    inputEmail.add(email);
+    loginObject = loginObject.copyWith(email: email);
+    inputEmailIcon.add(_isEmailValid(email));
+    _validate();
+  }
+
+  @override
+  Stream<bool> get outputIsPasswordValid => _passwordStreamController.stream
+      .map((password) => _isPasswordValid(password));
+
+  @override
+  Stream<bool> get outputIsEmailValid =>
+      _emailStreamController.stream.map((email) => _isEmailValid(email));
+
+  @override
+  Stream<bool> get outputIsAllInputsValid =>
+      _isAllInputsValidStreamController.stream.map((_) => _isAllInputsValid());
+
+  @override
+  Stream<bool> get outputPasswordVisibility =>
+      _passwordVisibilityStreamController.stream;
+
+  @override
+  Stream<bool> get outputEmailIcon => _emailIconStreamController.stream;
+
+  void togglePasswordVisibility() {
+    _isPasswordHidden = !_isPasswordHidden;
+    inputPasswordVisibility.add(_isPasswordHidden);
+  }
+
+  _validate() {
+    inputIsAllInputValid.add(null);
+  }
+
+  bool _isPasswordValid(String password) {
+    return password.isNotEmpty;
+  }
+
+  bool _isEmailValid(String email) {
+    final RegExp emailRegex = RegExp(RegexValidation.emailRegex);
+    return emailRegex.hasMatch(email);
+  }
+
+  bool _isAllInputsValid() {
+    return _isPasswordValid(loginObject.password) &&
+        _isEmailValid(loginObject.email);
+  }
+}
+
+mixin LoginViewModelInputs {
+  setEmail(String email);
+
+  setPassword(String password);
+
+  login();
+
+  Sink get inputEmail;
+
+  Sink get inputPassword;
+
+  Sink get inputIsAllInputValid;
+
+  Sink get inputPasswordVisibility;
+
+  Sink get inputEmailIcon;
+}
+
+mixin LoginViewModelOutputs {
+  Stream<bool> get outputIsEmailValid;
+
+  Stream<bool> get outputIsPasswordValid;
+
+  Stream<bool> get outputIsAllInputsValid;
+
+  Stream<bool> get outputPasswordVisibility;
+
+  Stream<bool> get outputEmailIcon;
+}
