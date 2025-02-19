d76e40b8be10deb58cea31864baa79f155a35975
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-02-19 16:12:56 +0200

    Added input state and output state to view model

--- lib/presentation/base/base_viewmodel.dart
+++ lib/presentation/base/base_viewmodel.dart
@@ -1,8 +1,31 @@
-abstract class BaseViewModel with BaseViewModelInputs, BaseViewModelOutputs {}
+import 'dart:async';
+
+import 'package:globenotes/presentation/common/state_renderer/state_renderer_impl.dart';
+
+abstract class BaseViewModel with BaseViewModelInputs, BaseViewModelOutputs {
+  final StreamController _inputStateStreamController =
+      StreamController<FlowState>.broadcast();
+
+  @override
+  Sink get inputState => _inputStateStreamController.sink;
+
+  @override
+  Stream<FlowState> get outputState =>
+      _inputStateStreamController.stream.map((flowState) => flowState);
+
+  @override
+  void dispose() {
+    _inputStateStreamController.close();
+  }
+}
 
 mixin BaseViewModelInputs {
   void start();
   void dispose();
+
+  Sink get inputState;
 }
 
-mixin BaseViewModelOutputs {}
+mixin BaseViewModelOutputs {
+  Stream<FlowState> get outputState;
+}
