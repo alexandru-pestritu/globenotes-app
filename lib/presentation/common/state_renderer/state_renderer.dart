1dad37e09cbb79030c4ff04c79c236713fbf6fa9
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-02-19 16:12:25 +0200

    State renderer

---
+++ lib/presentation/common/state_renderer/state_renderer.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:globenotes/app/extensions.dart';
+import 'package:lottie/lottie.dart';
+import 'package:globenotes/presentation/resources/assets_manager.dart';
+import 'package:globenotes/presentation/resources/values_manager.dart';
+import 'package:globenotes/presentation/resources/strings_manager.dart';
+
+enum StateRendererType {
+  fullScreenLoadingState,
+  fullScreenErrorState,
+  contentScreenState,
+  emptyScreenState,
+
+  snackbarErrorState,
+  snackbarSuccessState,
+}
+
+class StateRenderer extends StatelessWidget {
+  final StateRendererType stateRendererType;
+  final String message;
+  final String title;
+  final Function? retryActionFunction;
+
+  const StateRenderer({
+    super.key,
+    required this.stateRendererType,
+    required this.message,
+    this.title = empty,
+    required this.retryActionFunction,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return _getStateWidget(context);
+  }
+
+  Widget _getStateWidget(BuildContext context) {
+    switch (stateRendererType) {
+      case StateRendererType.fullScreenLoadingState:
+        return _getItemsInColumn([
+          _getAnimatedImage(animationPath: JsonAssets.loading),
+        ]);
+
+      case StateRendererType.fullScreenErrorState:
+        return _getItemsInColumn([
+          _getAnimatedImage(animationPath: JsonAssets.error, repeat: false),
+          _getMessage(message, context),
+          _getRetryButton(AppStrings.retry, context),
+        ]);
+
+      case StateRendererType.emptyScreenState:
+        return _getItemsInColumn([
+          _getAnimatedImage(animationPath: JsonAssets.empty, repeat: false),
+          _getMessage(message, context),
+        ]);
+
+      case StateRendererType.contentScreenState:
+      case StateRendererType.snackbarErrorState:
+      case StateRendererType.snackbarSuccessState:
+        return Container();
+    }
+  }
+
+  Widget _getItemsInColumn(List<Widget> children) {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: children,
+        ),
+      ),
+    );
+  }
+
+  Widget _getAnimatedImage({
+    required String animationPath,
+    double? size,
+    bool? repeat,
+  }) {
+    return SizedBox(
+      height: size ?? AppSize.s150,
+      width: size ?? AppSize.s150,
+      child: Lottie.asset(animationPath, repeat: repeat ?? true),
+    );
+  }
+
+  Widget _getMessage(String message, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(AppPadding.p18),
+      child: Text(
+        message,
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
+    );
+  }
+
+  Widget _getRetryButton(String buttonTitle, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(AppPadding.p18),
+      child: SizedBox(
+        width: AppSize.s200,
+        child: ElevatedButton(
+          onPressed: () {
+            retryActionFunction?.call();
+          },
+          child: Text(buttonTitle),
+        ),
+      ),
+    );
+  }
+}
