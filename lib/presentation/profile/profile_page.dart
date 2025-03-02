cc010302dec112ea0a59a964c04a3a4dd359dda1
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-03-02 16:25:31 +0200

    Added drift database viewer

--- lib/presentation/profile/profile_page.dart
+++ lib/presentation/profile/profile_page.dart
@@ -1,6 +1,10 @@
+import 'package:drift_db_viewer/drift_db_viewer.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:globenotes/app/di.dart';
+import 'package:globenotes/data/database/app_database.dart';
 import 'package:globenotes/presentation/resources/strings_manager.dart';
+import 'package:globenotes/presentation/resources/values_manager.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,10 +14,23 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  AppDatabase db = instance<AppDatabase>();
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(AppStrings.profile.tr()),
+      child: SizedBox(
+        width: double.infinity,
+        height: AppSize.s45,
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(
+              context,
+            ).push(MaterialPageRoute(builder: (context) => DriftDbViewer(db)));
+          },
+          child: Text(AppStrings.profile.tr()),
+        ),
+      ),
     );
   }
 }
