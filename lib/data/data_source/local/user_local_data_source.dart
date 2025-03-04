bafedbb4ead9e2a467ad673b6a3aeb874fe60d74
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-03-04 19:17:07 +0200

    Updated insertUser method to check for existing user with same serverId before adding a new user

--- lib/data/data_source/local/user_local_data_source.dart
+++ lib/data/data_source/local/user_local_data_source.dart
@@ -1,7 +1,7 @@
 import 'package:globenotes/data/database/app_database.dart';
 
 abstract class UsersLocalDataSource {
-  Future<int> insertUser(UsersLocalCompanion user);
+  Future<UsersLocalData> insertUser(UsersLocalCompanion user);
   Future<List<UsersLocalData>> getAllUsers();
   Future<UsersLocalData> getUser(int localId);
   Future<bool> updateUser(UsersLocalData user);
@@ -15,9 +15,24 @@ class UsersLocalDataSourceImpl implements UsersLocalDataSource {
   UsersLocalDataSourceImpl(this._db);
 
   @override
-  Future<int> insertUser(UsersLocalCompanion user) {
-    return _db.into(_db.usersLocal).insert(user);
+  Future<UsersLocalData> insertUser(UsersLocalCompanion user) async {
+  if (user.serverId.present && user.serverId.value != null) {
+    final existingUser = await (_db.select(_db.usersLocal)
+          ..where((tbl) => tbl.serverId.equals(user.serverId.value!)))
+        .getSingleOrNull();
+
+    if (existingUser != null) {
+      return existingUser;
+    }
   }
+
+  final userId = await _db.into(_db.usersLocal).insert(user);
+
+  return await (_db.select(_db.usersLocal)
+        ..where((tbl) => tbl.localId.equals(userId)))
+      .getSingle();
+}
+
 
   @override
   Future<List<UsersLocalData>> getAllUsers() {
