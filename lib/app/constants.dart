fed76e77fa8b88b180e2e6541b4b52b993bd498a
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-03-14 19:00:10 +0200

    Changed api url

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const String baseUrl = "http://100.66.65.82:8080/api";
+  static const String baseUrl = "http://100.114.37.87:8080/api";
   static const String driftDatabaseName = "globenotes_db.sqlite";
   static const String webGoogleClientId =
       "1006453621944-8860hmcm3sa4l09qtfv12kdj1vgoeddn.apps.googleusercontent.com";
