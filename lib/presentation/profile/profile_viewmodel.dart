0e7acc55f472f77660610fb0f8d3b0ca16916062
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-03-21 15:52:32 +0200

    Profile page view model

---
+++ lib/presentation/profile/profile_viewmodel.dart
@@ -0,0 +1,59 @@
+import 'package:globenotes/domain/usecase/user/get_user_profile_usecase.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:globenotes/domain/model/model.dart';
+import 'package:globenotes/presentation/base/base_viewmodel.dart';
+import 'package:globenotes/presentation/common/state_renderer/state_renderer.dart';
+import 'package:globenotes/presentation/common/state_renderer/state_renderer_impl.dart';
+
+class ProfileViewModel extends BaseViewModel
+    with ProfileViewModelInputs, ProfileViewModelOutputs {
+  final GetUserProfileUseCase _getUserProfileUseCase;
+
+  final BehaviorSubject<UserProfile?> _profileSubject =
+      BehaviorSubject<UserProfile?>();
+
+  ProfileViewModel(this._getUserProfileUseCase);
+
+  @override
+  void start() {
+    fetchUserProfile();
+  }
+
+  @override
+  void dispose() {
+    _profileSubject.close();
+    super.dispose();
+  }
+
+  @override
+  void fetchUserProfile() async {
+    inputState.add(
+      LoadingState(),
+    );
+
+    final result = await _getUserProfileUseCase.execute(null);
+    result.fold(
+      (failure) {
+        inputState.add(
+          ErrorState(StateRendererType.fullScreenErrorState, failure.message),
+        );
+        _profileSubject.add(null);
+      },
+      (profile) {
+        inputState.add(ContentState());
+        _profileSubject.add(profile);
+      },
+    );
+  }
+
+  @override
+  ValueStream<UserProfile?> get outputUserProfile => _profileSubject.stream;
+}
+
+mixin ProfileViewModelInputs {
+  void fetchUserProfile();
+}
+
+mixin ProfileViewModelOutputs {
+  ValueStream<UserProfile?> get outputUserProfile;
+}
